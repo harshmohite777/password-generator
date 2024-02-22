6da257dbc298cee046d6a6c3f49d5610cff14108
harshmohite777 <harshsmohite05@gmail.com>
2024-02-23 00:28:58 +0530

    added all file

---
+++ script.js
@@ -0,0 +1,9 @@
+function generatePassword() {
+    var length = document.getElementById("length").value,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("password").innerText = retVal;
+}
